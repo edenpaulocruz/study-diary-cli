339e34bafd4bab3d5d608512728d133f5e9a330d
edenpaulocruz <edenpaulo@gmail.com>
2021-11-21 01:08:30 -0300

    Listando por categoria, refatorando

--- study_diary.rb
+++ study_diary.rb
@@ -4,7 +4,8 @@ require_relative 'study_item'
 ADD_ITEM = 1
 SHOW_ITEMS= 2
 SEARCH_ITEM = 3
-EXIT = 4
+SHOW_BY_CATEGORY = 4
+EXIT = 5
 
 def welcome
   puts "Bem-vindo ao Diário de Estudos, seu companheiro para estudar!"
@@ -15,6 +16,7 @@ def menu
   puts "[#{ADD_ITEM}] Cadastrar um item para estudar"
   puts "[#{SHOW_ITEMS}] Ver todos os itens cadastrados"
   puts "[#{SEARCH_ITEM}] Buscar um item de estudo"
+  puts "[#{SHOW_BY_CATEGORY}] Listar por categoria"
   puts "[#{EXIT}] Sair"
   puts
   print "Escolha uma opção: "
@@ -28,25 +30,21 @@ def continue
 end
 
 def add_item
-  categories = [
-    Category.new(1, 'HTML'),
-    Category.new(2, 'CSS'),
-    Category.new(3, 'JavaScript'),
-    Category.new(4, 'React'),
-    Category.new(5, 'Ruby'),
-    Category.new(6, 'Rails'),
-    Category.new(7, 'Golang')
-  ]
   puts
   print "Digite o título do seu item de estudo: "
   title = gets.chomp()
   puts
-  categories.each { |category| puts "##{category.id} - #{category.name}" }
-  print "Escolha uma categoria para o seu item de estudo: "
-  choice = gets.chomp()
-  category = categories.find { |item| item.id == choice.to_i }
+  category = select_category()
   puts
   StudyItem.new(title, category)
+end
+
+def select_category
+  puts
+  @categories.each { |category| puts "##{category.id} - #{category.name}" }
+  print "Escolha uma categoria: "
+  choice = gets.chomp()
+  category = @categories.find { |item| item.id == choice.to_i }
 end
 
 def show_items(items)
@@ -55,16 +53,38 @@ def show_items(items)
   items.each { |item| puts "#{item.title} - #{item.category.name}" }
 end
 
-def search_item(items)
+def search_items(items)
   puts
   print "Digite uma palavra para procurar: "
   word = gets.chomp().upcase
-  search_result = items.select { |item| item.title.upcase.include?(word) }
+  search_result(items, word)
+end
+
+def search_result(items, search)
+  if search.class == String
+    search_result = items.select { |item| item.title.upcase.include?(search) }
+  elsif search.class == Category
+    search_result = items.select { |item| item.category == search }
+  end
   puts
   puts "Foram encontrados #{search_result.length} itens."
   show_items(search_result)
 end
 
+def show_by_category(items)
+  category = select_category()
+  search_result(items, category)
+end
+
+@categories = [
+  Category.new(1, 'HTML'),
+  Category.new(2, 'CSS'),
+  Category.new(3, 'JavaScript'),
+  Category.new(4, 'React'),
+  Category.new(5, 'Ruby'),
+  Category.new(6, 'Rails'),
+  Category.new(7, 'Golang')
+]
 study_items = []
 welcome()
 
@@ -77,7 +97,10 @@ loop do
     show_items(study_items)
     continue()
   elsif option == SEARCH_ITEM
-    search_item(study_items)
+    search_items(study_items)
+    continue()
+  elsif option == SHOW_BY_CATEGORY
+    show_by_category(study_items)
     continue()
   elsif option == EXIT
     break
